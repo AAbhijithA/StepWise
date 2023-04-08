afdf436f005327ab0cbdcd0aacad5601fdbd7b19
Abhijith Ajith <113827354+AAbhijithA@users.noreply.github.com>
2023-04-08 20:31:48 +0530

    Image detection via yolov4-tiny with tensorflow and opencv

--- imgdet.py
+++ imgdet.py
@@ -9,7 +9,7 @@ import pandas as pd
 from datetime import date
 import time
 
-url='http://192.168.41.222/cam-hi.jpg'
+url='Enter the url link from the ESP32-Loader code'
 im=None
 def run1():
     cv2.namedWindow("live transmission", cv2.WINDOW_AUTOSIZE)
